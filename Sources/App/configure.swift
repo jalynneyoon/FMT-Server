c35b15fd07c812d304cbdd0d35bd73aeed09ba83
johyeon yoon <yds03080@naver.com>
2021-08-02 19:29:51 +0900

    Add postgreSQL dependency in SPM

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -7,12 +7,13 @@ public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    app.databases.use(.postgres(
+    app.databases.use(
+        .postgres(
         hostname: Environment.get("DATABASE_HOST") ?? "localhost",
         port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database"
+        username: "johyeonyoon",
+        password: "",
+        database: "five_mins_tracker_database"
     ), as: .psql)
 
     app.migrations.add(CreateTodo())
