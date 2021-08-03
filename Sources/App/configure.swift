2f431db98bf91df5df748bf115f5af35e323f6ea
johyeon yoon <yds03080@naver.com>
2021-08-03 21:34:01 +0900

    Configure heroku postgres database

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -7,21 +7,21 @@ public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    app.databases.use(
-        .postgres(
-        hostname: "localhost",
-        username: "johyeonyoon",
-        password: "",
-        database: "five_mins_tracker_database"
-    ), as: .psql)
-
     app.migrations.add(CreateUser())
     app.migrations.add(CreateHabits())
     app.migrations.add(CreateCheckList())
     app.migrations.add(CreateDoneLists())
     app.migrations.add(CreateLikes())
     
-
-    // register routes
+    
+    if let databaseURL = Environment.get("DATABASE_URL"), var postgresConfig = PostgresConfiguration(url: databaseURL) {
+        var clientTLSConfiguration = TLSConfiguration.makeClientConfiguration()
+        clientTLSConfiguration.certificateVerification = .none
+        postgresConfig.tlsConfiguration = clientTLSConfiguration
+        app.databases.use(.postgres(configuration: postgresConfig), as: .psql)
+    } else {
+        throw Abort(.internalServerError)
+    }
+    
     try routes(app)
 }
